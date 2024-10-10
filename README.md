ce1fb0df181914f5fc2c184d987c752cb9c0b3bb
Gabriel dos Santos <gabrields1910@gmail.com>
2024-10-10 18:29:26 -0300

    update dockercompose, env and prisma

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # crud-node-prisma
+
 API made in Node.JS and Prisma, using SOLID fundamentals
+
+Para iniciar o projeto
+
+npm i
+npx prisma generate
+npx prisma studio
+npm run dev
--- src/repositories/studant.repository.ts
+++ src/repositories/studant.repository.ts
@@ -1,50 +1,53 @@
 import { prisma } from '../database/prisma-client';
-import { Student, StudentCreate, StudentRepository } from '../interfaces/studants.interface';
+import {
+  Student,
+  StudentCreate,
+  StudentRepository,
+} from '../interfaces/studants.interface';
 
 class StudentRepositoryPrisma implements StudentRepository {
-    async create(data: StudentCreate): Promise<Student> {
-        const result = await prisma.studant.create({
-            data: {
-                name: data.name,
-                cpf: data.cpf,
-                ra: data.ra,
-                email: data.email
-            },
-        });
-        return result;
-    }
+  async create(data: StudentCreate): Promise<Student> {
+    const result = await prisma.student.create({
+      data: {
+        name: data.name,
+        cpf: data.cpf,
+        ra: data.ra,
+        email: data.email,
+      },
+    });
+    return result;
+  }
 
-    async findByCpfOrRa(cpfOrRa: string): Promise<Student | null> {
-        const result = await prisma.studant.findFirst({
-            where: {
-                OR: [{ cpf: cpfOrRa }, { ra: cpfOrRa }],
-            },
-        })
-        return result
-    }
+  async findByCpfOrRa(cpfOrRa: string): Promise<Student | null> {
+    const result = await prisma.student.findFirst({
+      where: {
+        OR: [{ cpf: cpfOrRa }, { ra: cpfOrRa }],
+      },
+    });
+    return result;
+  }
 
-    async listAllStudents(): Promise<Student[]> {
-        const result = await prisma.studant.findMany()
-        return result
-    }
+  async listAllStudents(): Promise<Student[]> {
+    const result = await prisma.student.findMany();
+    return result;
+  }
 
-    async updateStudent({ id, name, email }: Student): Promise<Student> {
-        const result = await prisma.studant.update({
-            where: { id },
-            data: { name, email }
-        })
-        return result
-    }
+  async updateStudent({ id, name, email }: Student): Promise<Student> {
+    const result = await prisma.student.update({
+      where: { id },
+      data: { name, email },
+    });
+    return result;
+  }
 
-    async delete(id: string): Promise<Student> {
-        const result = await prisma.studant.delete({
-            where: {
-                id
-            }
-        })
-        return result
-    }
+  async delete(id: string): Promise<Student> {
+    const result = await prisma.student.delete({
+      where: {
+        id,
+      },
+    });
+    return result;
+  }
 }
 
 export { StudentRepositoryPrisma };
-
